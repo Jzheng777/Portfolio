10266808c4c5b3bc1a8c3976e60410907cb544d2
Jzheng777 <sniggy777@yahoo.com>
2024-05-21 09:33:39 -0400

    Made some mobile responsiveness updates

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -10,16 +10,18 @@ const Projects: React.FC = () => {
       <h2>&lt;Project&gt;</h2>
       <div className='project-wrap'><img src={powerChess} alt="Power-Chess-Image" />
         <div className='text'>
-        <span className="project-name">*Power Chess*</span> <p>
-            Created a multiplayer Chinese Chess web application
+        <a href="https://www-student.cse.buffalo.edu/CSE442-542/2024-Spring/cse-442ad/dist/#/" target="_blank" rel="noopener noreferrer" className="project-link">
+          <span className="project-name">*Power Chess*</span> </a>
+          <p> Created a multiplayer Chinese Chess web application
         that you can play with friends, randoms, or even solo!</p>
         <p><b>Tech stack:</b> Vite, Php, MySQL, Apache</p>
         </div>
       </div>
       <div className='project-wrap'><img src={teamUp} alt="Team-Up-Image" />
         <div className='text'>
-          <span className="project-name">*Team Up*</span><p>
-          A gaming forum that displays the top 5 streamers of specific games that you choose.
+        <a href="https://webdev.cse.buffalo.edu/hci/teams/ok/" target="_blank" rel="noopener noreferrer" className="project-link">
+          <span className="project-name">*Team Up*</span> </a>
+          <p> A gaming forum that displays the top 5 streamers of specific games that you choose.
           There is a level and ranking system called xp that correlates to the likes you have in total for your posts.
           You can also find other players that play the same games as you.</p>
           <p><b>Tech stack:</b> React, Twitch API, MySQL, Php</p>
@@ -27,8 +29,9 @@ const Projects: React.FC = () => {
       </div>
        <div className='project-wrap'><img src={bottomHat} alt="Bottom-Hat-Image" />
         <div className='text'>
-          <span className="project-name">*Bottom Hat*</span><p>
-            A quiz creation/maker web application where you can make your own
+        <a href="https://bottomhat.net/" target="_blank" rel="noopener noreferrer" className="project-link">
+          <span className="project-name">*Bottom Hat*</span></a>
+          <p>A quiz creation/maker web application where you can make your own
           quizzes and time limit or you can answer other people's quiz questions.</p>
           <p><b>Tech stack:</b> Bootstrap, Flask-Python, Socket.io, Nginx, Docker, MongoDB</p>
         </div>
